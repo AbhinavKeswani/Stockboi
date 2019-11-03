d56b3613151c81e6a9bd15564ced49d3b9851859
AK324 <abhinavkeswani4@gmail.com>
2019-11-03 02:33:31 -0500

    saving progress, basic backbone done, inputting canvas now

--- GUI/OptionPane.py
+++ GUI/OptionPane.py
@@ -1,28 +1,15 @@
-def start():
-	root = Tk()
-	canvas = Canvas(root, width=1800, height = 1050)
-	Button = Button(root, text= "Click to Start")
-	Button.pack()
-	Button.place(x=598, y=824)
-	button.
-	root.mainloop()
-
-def invadv():
-	canvas = Canvas(root, width=1800, height = 1050)
-	root = Tk()
-	frame = Frame(root)
-	Button = Button(root, text= "Investment Advice")
-	root.mainloop()
-
+started = 0.0
 def moneyinvest(): 
 	canvas = Canvas(root, width=1800, height = 1050)
 	root = Tk()
+	root.geometry("1800x1050")
 	frame = Frame(root)
-	Button = Button(root, text= "How much money do you want to invest?")
+	invest = Entry(root, text= "How much money do you want to invest?")
 	root.mainloop()
 
 def lt(): #TODO: Use functions for machine learning but long term here
 	root = Tk()
+	root.geometry("1800x1050")
 	canvas = Canvas(root, width=1800, height = 1050)
 	frame = Frame(root)
 	text1 = Text(top)
@@ -33,6 +20,7 @@ def lt(): #TODO: Use functions for machine learning but long term here
 
 def st(): #TODO: Use functions for machine learning but short term here
 	root = Tk()
+	root.geometry("1800x1050")
 	canvas = Canvas(root, width=1800, height = 1050)
 	frame = Frame(root)
 	text2 = Text(top)
@@ -43,6 +31,7 @@ def st(): #TODO: Use functions for machine learning but short term here
 
 def stlt():
 	root = Tk()
+	root.geometry("1800x1050")
 	canvas = Canvas(root, width=1800, height = 1050)
 	frame = Frame(root)
 	LongTerm = Button(root, text="Long-Term")
@@ -56,6 +45,7 @@ def stlt():
 def v():
 	frame = Frame(root)
 	root = Tk()
+	root.geometry("1280x720")
 	canvas = Canvas(root, width=1800, height = 1050)
 	hv = Button(root)
 	hv.pack()
@@ -65,9 +55,45 @@ def v():
 	root.mainloop()
 
 
+
 from Tkinter import *
 from PIL import ImageTk, Image
 
-start()
+def LV():
+	print('longer yay')
+
+def HV(): #Change 
+	HV = Tk()
+	HV.geometry("1280x720")
+
+
+def Volatility():
+	yeet = Tk()
+	yeet.geometry("1280x720")
+	frame = Frame(yeet)
+	Button1 = Button(yeet, text= "High Volatility")
+	Button1.pack()
+	Button2 = Button(yeet, text="Low Volatility")
+	Button2.pack()
+	Button1.config(command=HV)
+	Button2.config(command=LV)
+	root.mainloop()
+
+def Starting():
+	yeet = Tk()
+	yeet.geometry("1280x720")
+	C = Canvas(yeet, height=1280, width = 720)
+	button2 = Button(yeet, text="Get Investing Advice")
+	button2.pack()
+	button2.config(command=Volatility)
+	yeet.mainloop()
+
+#actual code is one line lol
+Starting()
+
+
+
+
+
 
 
